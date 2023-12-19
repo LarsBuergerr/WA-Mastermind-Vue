6f4eaa6c22078e5c5f4d41554d64a1b63b3610a1
Smokey95 <97231524+Smokey95@users.noreply.github.com>
2023-12-19 23:25:49 +0100

    10-WebComponents - small alignment changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -58,152 +58,149 @@
 </script>
 
 <style scoped>
-/* Apply settings so navbar is transparent */
-.navbar {
-  z-index: 9999;
-  background-color: transparent !important;
-  padding-top: 1rem;
-  padding-left: 1rem;
-}
 
-/* Change navbar icon size */
-.navbar-icon {
-  z-index: 9999;
-  width: 5rem;
-  height: 5rem;
-}
-
-/* Padding for the navbar icon*/
-.navbar-toggler {
-  z-index: 9999;
-  margin-left: auto; /* Automatically align the navbar icon to the right */
-  padding-top: 1rem;
-  padding-right: 1rem;
-  font-size: 2rem;
-  border: none;
-  background-color: transparent;
-}
-
-.nav-link {
-  z-index: 9999;
-  font-size: 2rem;
-  color: aliceblue !important;
-}
-
-.nav-icon{
-  z-index: 9999;
-  width: 3rem;
-  height: 3rem;
-  margin-left: 1rem;
-  display: inline-block;
-  margin-right: 1rem;
-}
-
-.nav-link:hover {
-  z-index: 9999;
-  background: linear-gradient(to bottom, red, rgb(56, 56, 252)); /* Apply linear gradient to the text color */
-  -webkit-background-clip: text; /* Set the background clip to text for webkit browsers */
-  background-clip: text;
-  -webkit-text-fill-color: transparent; /* Set the text fill color to transparent for webkit browsers */
-}
-
-.offcanvas {
-  z-index: 9999;
-  background-color: rgba(41, 41, 41, 0.8); /* black with 80% opacity */
-}
-
-.offcanvas-title {
-  z-index: 9999;
-  font-size: 2rem;
-}
-
-.offcanvas-header{
-  z-index: 9999;
-  color: aliceblue !important;
-  margin-left: 1rem;
-}
-
-.offcanvas .btn-close{
-  z-index: 9999;
-  color: aliceblue !important;
-  font-size: 2rem;
-}
-
-
-.orientationMessage{
-  z-index: 9999;
-  display: none;
-  position: absolute;
-  z-index: 9999;
-  background-color: rgba(41, 41, 41, 0.8);
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  text-align: center;
-
-  padding-top:2rem ;
-
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-}
-
-.orientationText {
-  z-index: 9999;
-  font-size: 2rem;
-  background: linear-gradient(to bottom, red, rgb(56, 56, 252));
-  -webkit-background-clip: text;
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
-
-.orientation-icon{
-  z-index: 9999;
-  width: 15rem;
-  animation: wiggle 1s infinite;
-}
-
-@keyframes wiggle {
-  0% { transform: rotate(-5deg); }
-  50% { transform: rotate(5deg); }
-  100% { transform: rotate(-5deg); }
-}
-
-/* Increase font size and icon size on small screens */
-@media (max-width: 1080px) {
-
-  .offcanvas {
-    --bs-offcanvas-width: 100%;
-  }
-
-  .offcanvas-title{
-    font-size: 5rem;
-  }
-
-  .offcanvas .btn-close {
-    color: aliceblue !important;
-    font-size: 5rem;
-  }
-
-  .nav-link {
-    font-size: 5rem; /* Increase font size on small screens */
-  }
-
-  .nav-icon {
-    width: 6rem; /* Increase icon width on small screens */
-    height: 6rem; /* Increase icon height on small screens */
-  }
-
-  /* Padding for the navbar icon*/
-  .navbar-toggler {
+  .navbar {
+    z-index: 9999;
+    background-color: transparent !important;
     padding-top: 1rem;
     padding-left: 1rem;
+  }
 
+  .navbar-icon {
+    z-index: 9999;
+    width: 5rem;
+    height: 5rem;
+  }
+
+  .navbar-toggler {
+    z-index: 9999;
+    margin-left: auto; /* Automatically align the navbar icon to the right */
+    padding-top: 1rem;
+    padding-right: 1rem;
+    font-size: 2rem;
     border: none;
     background-color: transparent;
   }
-} 
+
+  .nav-link {
+    z-index: 9999;
+    font-size: 2rem;
+    color: aliceblue !important;
+  }
+
+  .nav-icon{
+    z-index: 9999;
+    width: 3rem;
+    height: 3rem;
+    margin-left: 1rem;
+    display: inline-block;
+    margin-right: 1rem;
+  }
+
+  .nav-link:hover {
+    z-index: 9999;
+    background: linear-gradient(to bottom, red, rgb(56, 56, 252)); /* Apply linear gradient to the text color */
+    -webkit-background-clip: text; /* Set the background clip to text for webkit browsers */
+    background-clip: text;
+    -webkit-text-fill-color: transparent; /* Set the text fill color to transparent for webkit browsers */
+  }
+
+  .offcanvas {
+    z-index: 9999;
+    background-color: rgba(41, 41, 41, 0.8); /* black with 80% opacity */
+  }
+
+  .offcanvas-title {
+    z-index: 9999;
+    font-size: 2rem;
+  }
+
+  .offcanvas-header{
+    z-index: 9999;
+    color: aliceblue !important;
+    margin-left: 1rem;
+  }
+
+  .offcanvas .btn-close{
+    z-index: 9999;
+    color: aliceblue !important;
+    font-size: 2rem;
+  }
+
+  .orientationMessage{
+    z-index: 9999;
+    display: none;
+    position: absolute;
+    z-index: 9999;
+    background-color: rgba(41, 41, 41, 0.8);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+
+    padding-top:2rem ;
+
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+  }
+
+  .orientationText {
+    z-index: 9999;
+    font-size: 2rem;
+    background: linear-gradient(to bottom, red, rgb(56, 56, 252));
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  .orientation-icon{
+    z-index: 9999;
+    width: 15rem;
+    animation: wiggle 1s infinite;
+  }
+
+  @keyframes wiggle {
+    0% { transform: rotate(-5deg); }
+    50% { transform: rotate(5deg); }
+    100% { transform: rotate(-5deg); }
+  }
+
+  /* Increase font size and icon size on small screens */
+  @media (max-width: 1080px) {
+  
+    .offcanvas {
+      --bs-offcanvas-width: 100%;
+    }
+  
+    .offcanvas-title{
+      font-size: 5rem;
+    }
+  
+    .offcanvas .btn-close {
+      color: aliceblue !important;
+      font-size: 5rem;
+    }
+  
+    .nav-link {
+      font-size: 5rem; /* Increase font size on small screens */
+    }
+  
+    .nav-icon {
+      width: 6rem; /* Increase icon width on small screens */
+      height: 6rem; /* Increase icon height on small screens */
+    }
+  
+    /* Padding for the navbar icon*/
+    .navbar-toggler {
+      padding-top: 1rem;
+      padding-left: 1rem;
+    
+      border: none;
+      background-color: transparent;
+    }
+  } 
 </style>
