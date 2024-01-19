f63db0923f17c8fb5b21edd2ba1e2bd43366b232
lars buerger <lars.buerger@freenet.de>
2024-01-19 16:51:41 +0100

    fdfdsafdsa

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.Node_ENV === 'production' ? '/mastermind-vue/' : '/',
+  publicPath: process.env.Node_ENV === 'production' ? '/WA-Mastermind-Vue/' : '/',
   transpileDependencies: true
 })
